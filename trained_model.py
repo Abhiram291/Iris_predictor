7fc7430795a183a31111297a2f97a09222e3b6ba
“Abhiram291” <abhiramayla@gmail.com>
2025-07-18 16:10:54 +0530

    Update app.py and trained_model.py with latest changes

--- trained_model.py
+++ trained_model.py
@@ -1,4 +1,4 @@
-# train_model.py
+
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
@@ -8,23 +8,19 @@ import pickle
 df = pd.read_csv("/Users/abhiramayla/Downloads/Iris.csv")
 df.columns = df.columns.str.strip()
 
-
 df = df.drop('Id', axis=1)
 
-# ✅ Use correct feature columns
 feature_cols = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
 X = df[feature_cols]
 y = df["Species"]
 
-# Encode labels
 le = LabelEncoder()
 y_encoded = le.fit_transform(y)
 
 model = RandomForestClassifier()
 model.fit(X, y_encoded)
 
-# Save model and encoder
 with open("model.pkl", "wb") as f:
     pickle.dump((model, le), f)
 
-print("✅ Model and label encoder saved.")
+print("Model and label encoder saved.")
